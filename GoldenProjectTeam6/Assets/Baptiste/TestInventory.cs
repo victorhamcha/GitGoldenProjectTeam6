4573a169909a4cb885bf84dfb27006b1ea0e7f7b
Barreyre <barreyre26@gmail.com>
2020-05-04 20:52:21 +0200

    + Baptiste Trucs

---
+++ GoldenProjectTeam6/Assets/Baptiste/TestInventory.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestInventory : MonoBehaviour
+{
+    [Header("Invetaire")]
+
+    public bool _trueDamage;
+    public bool _isVirgin;
+    public bool _isPuceau;
+    public bool _electricityOn;
+    public bool _waterOn;
+    public bool _hasKeyRed;
+    public bool _hasScrewdriver;
+    public bool _isHot;
+    public bool _hasKey;
+}
