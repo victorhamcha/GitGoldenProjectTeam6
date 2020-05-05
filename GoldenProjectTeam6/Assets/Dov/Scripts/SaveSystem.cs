d0a9ec5192ea20bc0a486bf479490d200d6fe95d
Benzakk <dovbenzak2@hotmail.com>
2020-05-05 14:14:27 +0200

    save system

---
+++ GoldenProjectTeam6/Assets/Dov/Scripts/SaveSystem.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public static class SaveSystem
+{
+    public static void SaveScore (ScoreSystem score)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + "/player.fun";
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        PlayerData data = new PlayerData(score);
+
+        formatter.Serialize(stream, data);
+        stream.Close();
+    }
+
+    public static PlayerData LoadPlayer()
+    {
+        string path = Application.persistentDataPath + "/player.fun";
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            PlayerData data = formatter.Deserialize(stream) as PlayerData;
+            stream.Close();
+
+            return data;
+        }
+        else
+        {
+            Debug.Log("Save file not found in " + path);
+            return null;
+        }
+    }
+}
---
+++ GoldenProjectTeam6/Assets/Dov/Scripts/ScoreSystem.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreSystem : MonoBehaviour
+{
+    public int score = 0;
+    public Text scoreText;
+    private bool successTest = false;
+    private Touch touch;
+
+    void Update()
+    {
+        scoreText.text = score.ToString();
+        score += Mathf.RoundToInt(Time.deltaTime);
+
+        if (score == 10)
+        {
+            successTest = true;
+            Debug.Log(successTest);
+        }
+        if (score == 20)
+        {
+            successTest = false;
+            Debug.Log(successTest);
+        }
+    }
+
+    public void SavePlayer()
+    {
+        SaveSystem.SaveScore(this);
+    }
+
+    public void LoadPlayer()
+    {
+        PlayerData data = SaveSystem.LoadPlayer();
+        score = data.scoreData;
+
+        Vector3 position;
+        position.x = data.position[0];
+        position.y = data.position[1];
+        position.z = data.position[2];
+        transform.position = position;
+    }
+}
