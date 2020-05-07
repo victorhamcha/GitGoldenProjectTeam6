6a9ddc3727c4e149aeb2aac5cd37d2e5cbadbfa5
Paul Leduc <paulleduc72@gmail.com>
2020-05-07 12:55:46 +0200

    Modifs toggle censure

--- GoldenProjectTeam6/Assets/Dov/Scripts/GameManager.cs
+++ GoldenProjectTeam6/Assets/Dov/Scripts/GameManager.cs
@@ -1,8 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    
+    [HideInInspector] public Toggle _censuredToggle;
+    [HideInInspector] public bool _isCensuredMod;
+
+    public void ChangeToggle()
+    {
+        if (_censuredToggle.isOn)
+        {
+            _isCensuredMod = true;
+        }
+        else
+        {
+            _isCensuredMod = false;
+        }
+    }
 }
