6f467a03ca27b095a65b6cdd626d14fd2fb2c7a3
Benzakk <dovbenzak2@hotmail.com>
2020-05-16 14:03:36 +0200

    Modif save

--- GoldenProjectTeam6/Assets/Dov/Scripts/PlayerData.cs
+++ GoldenProjectTeam6/Assets/Dov/Scripts/PlayerData.cs
@@ -9,6 +9,7 @@ public class PlayerData
     public List<string> imageTreeUnlockSinceLastTimeData;
     public List<string> savingDrawCardCardData;
     public List<bool> optionsData;
+    
 
 
     public PlayerData (ContainAllObjectTree objectTree, GameManager cardsAlreadyDraw, PauseMenu saveOptions)
@@ -29,13 +30,21 @@ public class PlayerData
 public class CardsData
 {
     public string firstCardData;
+    public List<bool> allSuccesData;
 
-    public CardsData(CardValuesWithScriptable cardvalue)
+    public CardsData(CardValuesWithScriptable cardvalue, SuccesManager allSucces)
     {
 
         if (cardvalue != null)
         {
             firstCardData = cardvalue._firstCardScriptable.name;
         }
+
+
+        if (allSucces != null)
+        {
+           allSuccesData = allSucces.lockInfo;
+        }
+
     }
 }
