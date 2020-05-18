1816060873dec115d8f77f82a180bd0bcaf08308
Hamcha <victor.hamcha@gmail.com>
2020-05-18 18:25:05 +0200

    achievement Google

--- GoldenProjectTeam6/Assets/Dov/Scripts/TextControl.cs
+++ GoldenProjectTeam6/Assets/Dov/Scripts/TextControl.cs
@@ -1,37 +1,37 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using TMPro;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using TMPro;
 
-public class TextControl : MonoBehaviour
-{
-    public SpriteRenderer top;
-    public SpriteRenderer down;
-    public TextMeshProUGUI text;
+//public class TextControl : MonoBehaviour
+//{
+//    public SpriteRenderer top;
+//    public SpriteRenderer down;
+//    public TextMeshProUGUI text;
 
-    public int haut;
-    public int bas;
+//    public int haut;
+//    public int bas;
     
 
-    void Update()
-    {
+//    void Update()
+//    {
 
-        text.rectTransform.rect.position.y 
-        if (text.rectTransform.rect.height < 900)
-        {
-            top.enabled = true;
-        }
-        else
-        {
-            top.enabled = false;
-        }
-        if (text.rectTransform.rect.height < 1400 )
-        {
-            down.enabled = true;
-        }
-        else
-        {
-            down.enabled = false;
-        }
-    }
-}
+//        text.rectTransform.rect.position.y 
+//        if (text.rectTransform.rect.height < 900)
+//        {
+//            top.enabled = true;
+//        }
+//        else
+//        {
+//            top.enabled = false;
+//        }
+//        if (text.rectTransform.rect.height < 1400 )
+//        {
+//            down.enabled = true;
+//        }
+//        else
+//        {
+//            down.enabled = false;
+//        }
+//    }
+//}
