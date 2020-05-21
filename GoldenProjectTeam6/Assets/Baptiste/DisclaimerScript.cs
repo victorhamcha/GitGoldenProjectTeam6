4fcfaff40ca97a7dcb89a3dc2b2497264741f899
Barreyre <barreyre26@gmail.com>
2020-05-21 15:41:04 +0200

    Disclaimer added

---
+++ GoldenProjectTeam6/Assets/Baptiste/DisclaimerScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisclaimerScript : MonoBehaviour
+{
+    [SerializeField]
+    private float Timer;
+
+    private void Update()
+    {
+        Timer -= Time.deltaTime;
+        if (Timer <= 0/* || Fais le truc ou le mec touche l'écran*/)
+            SceneTransition();
+    }
+
+    private void SceneTransition()
+    {
+        // Fais le truc magique ou la scene change
+    }
+}
