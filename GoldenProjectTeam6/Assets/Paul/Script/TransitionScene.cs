c47c39e59446c569e469acfad6f1ccd24987df3b
Paul Leduc <paulleduc72@gmail.com>
2020-05-28 12:05:45 +0200

    Added Assets

---
+++ GoldenProjectTeam6/Assets/Paul/Script/TransitionScene.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransitionScene : MonoBehaviour
+{
+    public Animator _animator;
+    [Range(0, 1)] public float _animTime;
+    void Start()
+    {
+        StartAnim();
+    }
+
+    public void StartAnim()
+    {
+        _animator.gameObject.SetActive(true);
+        _animator.SetBool("IStartAnim", true);
+        StartCoroutine(WaitForMask());
+    }
+
+    IEnumerator WaitForMask()
+    {
+        yield return new WaitForSeconds(_animTime);
+        _animator.gameObject.SetActive(false);
+    }
+
+    public void EndAnim()
+    {
+        _animator.gameObject.SetActive(true);
+        _animator.SetBool("IStartAnim", false);
+    }
+}
