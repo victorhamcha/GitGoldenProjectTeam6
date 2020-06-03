c24ea415014190f950f9e92e8a1a9038b1b463b0
Benzakk <dovbenzak2@hotmail.com>
2020-06-03 20:34:16 +0200

    Fin Confetti Particle + Blood Particle

--- GoldenProjectTeam6/Assets/Dov/Scripts/SpawnParticle.cs
+++ GoldenProjectTeam6/Assets/Dov/Scripts/SpawnParticle.cs
@@ -1,13 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SpawnParticle : MonoBehaviour
 {
     public GameObject confettiFx;
 
+    public GameObject bloodFx;
+    public GameObject bloodImage;
+
+    
     public void ClickConfetti()
     {
         GameObject ob = Instantiate(confettiFx);
+        Destroy(ob, 6.0f);
+    }
+
+    public void ClickBlood()
+    {
+        GameObject ob = Instantiate(bloodFx);
+        Destroy(ob, 4.0f);
+        bloodImage.SetActive(true);
     }
 }
