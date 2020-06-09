6a124b6b4187e5b527d1a3fb18667a5d677e55ab
Benzakk <dovbenzak2@hotmail.com>
2020-06-09 15:47:14 +0200

    modif script

---
+++ GoldenProjectTeam6/Assets/Dov/Scripts/Spawn/SpawnRire.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnRire : MonoBehaviour
+{
+    
+    void Awake()
+    {
+        FindObjectOfType<AudioManager>().Play("SFX_DeathMourirDeRire");
+    }
+
+    
+}
